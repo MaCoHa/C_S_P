7598e38542e29314c657f373150c50ad1fe6fe26
coha <coha@itu.dk>
2024-02-21 10:39:16 +0100

    lecture 4

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,11 +5,12 @@
 #include "includes/Concurrent.hpp"
 #include "includes/Count_then_move.hpp"
 #include "includes/utils/inputGen.hpp"
+#include <math.h>
 
 using namespace std;
 int main(int argc, char *argv[]) 
 {
-    vector<pair<uint64_t, uint64_t>> tuples = InputGen::gen_input(400);
+    vector<pair<uint64_t, uint64_t>> tuples = InputGen::gen_input(pow(2, 24));
     //Concurrent::run(tuples, 6, 4);
     CountThenMove::run(tuples, 6, 4);
     return 0;
