083d0df753b0d1306ec8bff8d4829ce8b4179c89
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-04-15 15:40:24 +0200

    Fixed validate

--- Project2/c_plus_plus/src/mergesort.cpp
+++ Project2/c_plus_plus/src/mergesort.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <fstream> 
-
+#include <fstream>
 
 using namespace std;
 
-
-void merge(int array[] , int const left, int const mid,
-		int const right)
+void merge(int array[], int const left, int const mid,
+		   int const right)
 {
 	int const subArrayOne = mid - left + 1;
 	int const subArrayTwo = right - mid;
 
 	auto *leftArray = new int[subArrayOne],
-		*rightArray = new int[subArrayTwo];
+		 *rightArray = new int[subArrayTwo];
 
 	for (auto i = 0; i < subArrayOne; i++)
 		leftArray[i] = array[left + i];
@@ -22,34 +20,31 @@ void merge(int array[] , int const left, int const mid,
 	auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
 	int indexOfMergedArray = left;
 
-	while (indexOfSubArrayOne < subArrayOne
-		&& indexOfSubArrayTwo < subArrayTwo) {
-		if (leftArray[indexOfSubArrayOne]
-			<= rightArray[indexOfSubArrayTwo]) {
-			array[indexOfMergedArray]
-				= leftArray[indexOfSubArrayOne];
+	while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+	{
+		if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+		{
+			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
 			indexOfSubArrayOne++;
 		}
-		else {
-			array[indexOfMergedArray]
-				= rightArray[indexOfSubArrayTwo];
+		else
+		{
+			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
 			indexOfSubArrayTwo++;
 		}
 		indexOfMergedArray++;
 	}
 
-	
-	while (indexOfSubArrayOne < subArrayOne) {
-		array[indexOfMergedArray]
-			= leftArray[indexOfSubArrayOne];
+	while (indexOfSubArrayOne < subArrayOne)
+	{
+		array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
 		indexOfSubArrayOne++;
 		indexOfMergedArray++;
 	}
 
-
-	while (indexOfSubArrayTwo < subArrayTwo) {
-		array[indexOfMergedArray]
-			= rightArray[indexOfSubArrayTwo];
+	while (indexOfSubArrayTwo < subArrayTwo)
+	{
+		array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
 		indexOfSubArrayTwo++;
 		indexOfMergedArray++;
 	}
@@ -57,8 +52,7 @@ void merge(int array[] , int const left, int const mid,
 	delete[] rightArray;
 }
 
-
-void mergeSort(int array[] , int const begin, int const end)
+void mergeSort(int array[], int const begin, int const end)
 {
 	if (begin >= end)
 		return;
@@ -69,57 +63,58 @@ void mergeSort(int array[] , int const begin, int const end)
 	merge(array, begin, mid, end);
 }
 
-void printArray(int  A[], int size)
+void printArray(int A[], int size)
 {
 	for (int i = 0; i < size; i++)
 		cout << A[i] << " ";
 	cout << endl;
 }
-void validate(int A[], int size){
-    bool sorted = true;
-    for (int i = 1; i < size; i++){
-        if (A[i] > A[i - 1]){
-           sorted = false;
-        }
-        if (!sorted){ 
-            break;
-        }
-    }
-    cout << "The list is sorted: " << sorted << endl;
-
+void validate(int A[], int size)
+{
+	bool sorted = true;
+	for (int i = 1; i < size; i++)
+	{
+		if (A[i] < A[i - 1])
+		{
+			sorted = false;
+			break;
+		}
+	}
+	cout << "The list is sorted: " << sorted << endl;
 }
 
 int main(int argc, char *argv[])
 {
 	string path = ("../../data/");
 	path += argv[1];
-	
+
 	ifstream file(path);
-	
-    vector<int> numbers;
-    int num;
+
+	vector<int> numbers;
+	int num;
 	int numbers_size = 0;
-    while (file >> num) {
-        numbers.push_back(num);
+	while (file >> num)
+	{
+		numbers.push_back(num);
 		numbers_size = numbers_size + 1;
-    }
-
-
- 	file.close();
-
-    // Convert vector to array if needed
-    int array[numbers.size()];
-    for (size_t i = 0; i < numbers_size; ++i) {
-        array[i] = numbers[i];
-    }
-
-    validate(array,numbers_size-1);
-
-	if (*argv[2] == '1'){
-		mergeSort(array, 0, numbers_size-1);
 	}
-    validate(array,numbers_size-1);
+
+	file.close();
+
+	// Convert vector to array if needed
+	int array[numbers.size()];
+	for (size_t i = 0; i < numbers_size; ++i)
+	{
+		array[i] = numbers[i];
+	}
+
+	validate(array, numbers_size - 1);
+
+	if (*argv[2] == '1')
+	{
+		mergeSort(array, 0, numbers_size - 1);
+	}
+	validate(array, numbers_size - 1);
 
 	return 0;
 }
-
