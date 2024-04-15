cdc97abf7928d5a1ae8d338e1a9115dc10028643
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-04-15 12:23:39 +0200

    Fixed bash script

--- Project2/run.sh
+++ Project2/run.sh
@@ -36,7 +36,7 @@ done
 
 for i in 1 2 3 4 5 6 7 8
 do
-    mkdir ./tmp-data
+    [ ! -d "./tmp-data" ] && mkdir ./tmp-data
     for element_amount in "${million_elements[@]}"
     do
         echo "Making data for $element_amount"
@@ -61,7 +61,7 @@ do
                 perf stat -o output.txt -e cache-misses,dTLB-load-misses bash ./$language/run-$algorithm.sh $element_amount ./data/$element_amount.txt 1
                 grep -E 'cache-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n'   >> $cachemissBaseFile
                 grep -E 'dTLB-load-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n' >> $dTLBBaseFile
-                grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed'
+                grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed' >> $timingsBaseFile
             done
         done
     done
@@ -90,7 +90,7 @@ do
                 perf stat -o output.txt -e cache-misses,dTLB-load-misses bash ./$language/run-$algorithm.sh $element_amount ./data/$element_amount.txt 0
                 grep -E 'cache-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n'   >> $cachemissFile
                 grep -E 'dTLB-load-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n' >> $dTLBFile
-                grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed'
+                grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed' >> $timingsFile
             done
         done
     done
