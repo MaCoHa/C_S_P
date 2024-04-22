b31a8c3c3f48e996d6928759e7fe80ae18df73cf
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-04-22 22:01:23 +0200

    Removed first comma in files

--- Project2/run.sh
+++ Project2/run.sh
@@ -57,11 +57,16 @@ do
         timingsBaseFile="$folder/timings-base.csv"
         cachemissBaseFile="$folder/perf-cache-miss-base.csv"
         dTLBBaseFile="$folder/perf-dTLB-misses-base.csv"
+        baseCounter=1
         for element_amount in "${million_elements[@]}"
         do
-            echo -n "," >> $timingsBaseFile
-            echo -n "," >> $cachemissBaseFile
-            echo -n "," >> $dTLBBaseFile
+            if [ "${counter}" != "1" ]
+            then
+                echo -n "," >> $timingsBaseFile
+                echo -n "," >> $cachemissBaseFile
+                echo -n "," >> $dTLBBaseFile
+            fi
+            counter=$((counter +1))
             echo "Creating base case for $language with $element_amount million elements"
             perf stat -o output.txt -e cache-misses,dTLB-load-misses bash ./$language/run-$algorithm.sh $element_amount-1.txt 1
             grep -E 'cache-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n'   >> $cachemissBaseFile
@@ -93,17 +98,22 @@ do
                 timingsFile="$folder/timings-$dataType.csv"
                 cachemissFile="$folder/perf-cache-miss-$dataType.csv"
                 dTLBFile="$folder/perf-dTLB-misses-$dataType.csv"
+                counter=1
                 for element_amount in "${million_elements[@]}"
                 do
+                    if [ "${counter}" != "1" ]
+                    then
+                        echo -n "," >> $timingsFile
+                        echo -n "," >> $cachemissFile
+                        echo -n "," >> $dTLBFile
+                    fi
+                    counter=$((counter +1))
                     
-                    echo -n "," >> $timingsFile
-                    echo -n "," >> $cachemissFile
-                    echo -n "," >> $dTLBFile
                     echo "Running $algorithm with $language and $element_amount million elements for datatype: $datatype"
                     perf stat -o output.txt -e cache-misses,dTLB-load-misses bash ./$language/run-$algorithm.sh $element_amount-$dataType.txt 0
                     grep -E 'cache-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n'   >> $cachemissFile
                     grep -E 'dTLB-load-misses' output.txt | sed 's/[^0-9,]//g' | tr -d ',' | tr -d '\n' >> $dTLBFile
-                    grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed' >> $timingsFile
+                    grep -A 1 "seconds time elapsed" output.txt | tr -d 'seconds time elapsed' | tr -d '\n' >> $timingsFile
                 done
                 echo "" >> $timingsFile
                 echo "" >> $cachemissFile
@@ -114,7 +124,6 @@ do
     # Cleanup
     rm ./data
     rm output.txt
-    
 done
 
 duration=$SECONDS
